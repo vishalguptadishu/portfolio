974a00352e1680d6e7decebbb9c5f14365c8eb59
vishal gupta <vishalguptadishu@gmail.com>
2023-07-13 13:32:37 +0530

    final

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -15,8 +15,8 @@ const Projects = () => {
          <div className='container  ' >
             <div className='projects-box' >
             <div className='projects-box-unit'>
-              < img src="https://i.ibb.co/T06TCSQ/project1.png" alt="project1" border="0" />
-              <h2>Layar soppe</h2>
+              < img src="https://i.ibb.co/0qqzxCC/project1.png" alt="project1" border="0" />
+              <h2>MyCinema</h2>
               <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, delectus, molestiae praesentium neque doloremque rem eos voluptatum </p>
               <hr />
               <div className='skill-boxs'>
@@ -25,12 +25,12 @@ const Projects = () => {
                   <p>CSS</p>
               </div>
               <div className='projects-box-unit-button' >
-               <button><a href="https://github.com/vishalguptadishu/layers.shop.clone">Github</a> </button>
-               <button><a href="https://vishalguptadishu.github.io/layers.shop.clone/home.html">Live</a></button>
+               <a href="https://github.com/vishalguptadishu/MyCinema"><button>Github</button></a>
+               <a href="https://64afa19c84a38d29eea5caa4--spiffy-axolotl-0b1cff.netlify.app/"><button>Live</button></a>
               </div>
             </div>
             <div className='projects-box-unit'>
-              < img src="https://i.ibb.co/T06TCSQ/project1.png" alt="project1" border="0" />
+              < img src="https://i.ibb.co/Svng4Yv/project2.png" alt="project1" border="0" />
               <h2>Layar soppe</h2>
               <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, delectus, molestiae praesentium neque doloremque rem eos voluptatum </p>
               <hr />
@@ -40,12 +40,13 @@ const Projects = () => {
                   <p>CSS</p>
               </div>
               <div className='projects-box-unit-button' >
-               <button>Github</button>
-               <button>Live</button>
+              <a href="https://github.com/vishalguptadishu/layers.shop.clone"> <button>Github</button></a>
+              <a href="https://64afa6576b7aa42bb418a47c--spontaneous-cendol-75e292.netlify.app/home.html"><button>Live</button></a> 
               </div>
             </div> 
+            
             <div className='projects-box-unit'>
-              < img src="https://i.ibb.co/T06TCSQ/project1.png" alt="project1" border="0" />
+              < img src="https://i.ibb.co/52Lwk89/project3.png" alt="project1" border="0" />
               <h2>Layar soppe</h2>
               <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, delectus, molestiae praesentium neque doloremque rem eos voluptatum </p>
               <hr />
@@ -55,12 +56,12 @@ const Projects = () => {
                   <p>CSS</p>
               </div>
               <div className='projects-box-unit-button' >
-               <button>Github</button>
-               <button>Live</button>
+               <a href="https://github.com/vishalguptadishu/gym_website"><button>Github</button></a>
+               <a href="https://boisterous-dango-74662e.netlify.app/"><button>Live</button></a>
               </div>
             </div>
             <div className='projects-box-unit'>
-              < img src="https://i.ibb.co/T06TCSQ/project1.png" alt="project1" border="0" />
+              < img src="https://i.ibb.co/4dWPGq5/project4.png" alt="project1" border="0" />
               <h2>Layar soppe</h2>
               <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, delectus, molestiae praesentium neque doloremque rem eos voluptatum </p>
               <hr />
@@ -70,23 +71,8 @@ const Projects = () => {
                   <p>CSS</p>
               </div>
               <div className='projects-box-unit-button' >
-               <button>Github</button>
-               <button>Live</button>
-              </div>
-            </div>
-            <div className='projects-box-unit'>
-              < img src="https://i.ibb.co/T06TCSQ/project1.png" alt="project1" border="0" />
-              <h2>Layar soppe</h2>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, delectus, molestiae praesentium neque doloremque rem eos voluptatum </p>
-              <hr />
-              <div className='skill-boxs'>
-                  <p>javascripte</p>
-                  <p>HTML</p>
-                  <p>CSS</p>
-              </div>
-              <div className='projects-box-unit-button' >
-               <button>Github</button>
-               <button>Live</button>
+               <a href="https://github.com/vishalguptadishu/CLONE_TWITTER"><button>Github</button></a>
+               <a href="https://lustrous-horse-2e4aaa.netlify.app/"><button>Live</button></a>
               </div>
             </div>
 
